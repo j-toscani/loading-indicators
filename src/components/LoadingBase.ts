29af84740c1779c7cb99494064dab9ef80447a39
Julian Toscani <julian_toscani@gmx.de>
2021-06-20 23:24:42 +0200

    add webcomponent base

---
+++ src/components/LoadingBase.ts
@@ -0,0 +1,50 @@
+const LOADING_STATES = ["error", "loading", "success", "neutral"];
+
+export default class LoadingBase extends HTMLElement {
+  static get observedAttributes() {
+    return ["data-loading"];
+  }
+  constructor() {
+    super();
+    const templateId = this.dataset.templateId ?? "loading-template";
+    let template = document.getElementById(templateId) as HTMLTemplateElement;
+
+    this.classList.add("loading-container");
+
+    if (!template) {
+      throw Error("No Template!");
+    }
+
+    this.appendChild(template.content.cloneNode(true));
+    this.loadingState = "neutral";
+    this.setAttribute("data-loading", "neutral");
+  }
+
+  set loadingState(val: string) {
+    val = this.checkNewLoadingState(val);
+    this.setNodeStates(val);
+  }
+
+  attributeChangedCallback(_name: string, _oldValue: string, newValue: string) {
+    this.loadingState = newValue;
+  }
+
+  setNodeStates(newValue: string) {
+    const stateNodes = Array.from(this.children);
+
+    stateNodes.forEach((node) => {
+      const wasChosen = node.getAttribute("data-loading") === newValue;
+      node.classList[wasChosen ? "add" : "remove"]("visible");
+    });
+  }
+
+  checkNewLoadingState(value: string) {
+    if (!LOADING_STATES.includes(value)) {
+      value = "neutral";
+      console.warn(
+        "Using unsupported value. Resseting loading state to 'neutral'"
+      );
+    }
+    return value;
+  }
+}
